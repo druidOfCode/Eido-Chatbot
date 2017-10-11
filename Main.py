8cd5e168691dcf4e911dcc71653d2c899aea6b1a
Brian Walton <bckwalton@gmail.com>
2017-10-11 01:34:54 -0400

    Added Chatterbots twitter module
    It's commented out as the results are not as expected. looking into improving the module, Or just making my own.

--- Main.py
+++ Main.py
@@ -1,13 +1,38 @@
 from chatterbot import ChatBot
+import chatterbot
+from settings import TWITTER
 import time
+import logging
+
+
+#This turns on logs in terminal (Quite distracting)
+# logging.basicConfig(level=logging.INFO)
+
+#This is the twitter module, be sure to comment out the othe bot before using this!
+# chatbot = ChatBot(
+#    "TwitterBot",
+#    logic_adapters=[
+#        "chatterbot.logic.BestMatch"
+#    ],
+#    database="./twitter-database.db",
+#    twitter_consumer_key=TWITTER["CONSUMER_KEY"],
+#    twitter_consumer_secret=TWITTER["CONSUMER_SECRET"],
+#    twitter_access_token_key=TWITTER["ACCESS_TOKEN"],
+#    twitter_access_token_secret=TWITTER["ACCESS_TOKEN_SECRET"],
+#    trainer="chatterbot.trainers.TwitterTrainer",
+#)
+#chatbot.train()
+
 
 chatbot = ChatBot(
-    'Ron Obvious',
-    trainer='chatterbot.trainers.ChatterBotCorpusTrainer'
+    'Eido',
+    trainer='chatterbot.trainers.ChatterBotCorpusTrainer',
+    storage_adapter='chatterbot.storage.SQLStorageAdapter',
+    database='./chat-database.sqlite3',
+    logic_adapters=[
+        'chatterbot.logic.BestMatch'
+    ],
 )
-
-# Train based on the english corpus
-# chatbot.train("chatterbot.corpus.english")
 
 # More training
 chatbot.train("chatterbot.corpus.english.greetings")
