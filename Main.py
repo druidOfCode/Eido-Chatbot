015224ae9d76961625fe2705aa41d5484c115ce2
Brian Walton <bckwalton@gmail.com>
2018-03-05 18:51:35 -0500

    Added Speech Recognition
    Added speech recognition that piggybacks off of Google's robust speech library

--- Main.py
+++ Main.py
@@ -1,28 +1,10 @@
 from chatterbot import ChatBot
-import chatterbot
-from settings import TWITTER
 import time
-import logging
-
+# import logging
+import speech_recognition as sr
 
 # This turns on logs in terminal (Quite distracting)
 # logging.basicConfig(level=logging.INFO)
-
-# This is the twitter module, be sure to comment out the othe bot before using this!
-# chatbot = ChatBot(
-#    "TwitterBot",
-#    logic_adapters=[
-#        "chatterbot.logic.BestMatch"
-#    ],
-#    database="./twitter-database.db",
-#    twitter_consumer_key=TWITTER["CONSUMER_KEY"],
-#    twitter_consumer_secret=TWITTER["CONSUMER_SECRET"],
-#    twitter_access_token_key=TWITTER["ACCESS_TOKEN"],
-#    twitter_access_token_secret=TWITTER["ACCESS_TOKEN_SECRET"],
-#    trainer="chatterbot.trainers.TwitterTrainer",
-#)
-# chatbot.train()
-
 
 chatbot = ChatBot(
     'Eido',
@@ -37,14 +19,41 @@ chatbot = ChatBot(
 # More training
 chatbot.train("chatterbot.corpus.english.greetings")
 chatbot.train("chatterbot.corpus.english.conversations")
-
-# Get a response to an input statement
-while True:
-    item = input("Type Message: ")
-    if "end connection" in str(item):
-        print("Very well, ending connection to Construct")
-        time.sleep(2)
-        exit()
-    else:
-        print(chatbot.get_response("" + item))
-        time.sleep(2)
+query_answer = input("Please specify whether you want to type(1) or talk(2): ")
+if (query_answer == "2"):
+    r = sr.Recognizer()
+    m = sr.Microphone()
+    print("A moment of silence, please...")
+    with m as source:
+        r.adjust_for_ambient_noise(source)
+    print("Set minimum energy threshold to {}".format(r.energy_threshold))
+    with sr.Microphone() as source:
+        print("Speech recognition will begin in 3 second.")
+        time.sleep(3)
+        print("You may speak!")
+        while True:
+            audio = r.listen(source)
+            try:
+                item = r.recognize_google(audio)
+            except sr.UnknownValueError:
+                print("Could not understand audio")
+            except sr.RequestError as e:
+                print("Can't talk with servers right now; {0}".format(e))
+            if "end connection" in str(item):
+                print("Very well, ending connection to Construct")
+                time.sleep(2)
+                exit()
+            else:
+                print(chatbot.get_response("" + item))
+                time.sleep(2)
+else:
+    # Get a response to an input statement
+    while True:
+        item = input("Type Message: ")
+        if "end connection" in str(item):
+            print("Very well, ending connection to Construct")
+            time.sleep(2)
+            exit()
+        else:
+            print(chatbot.get_response("" + item))
+            time.sleep(2)
