9b5d2833b63df8559a1972a17c1d34e300d10a5e
Brian Walton <bckwalton@gmail.com>
2018-03-06 19:10:32 -0500

    Added some helper functions
    and some needed pauses so as to not confuse the user or the microphones

--- Main.py
+++ Main.py
@@ -18,11 +18,10 @@ chatbot = ChatBot(
 )
 
 # More training
-chatbot.train("chatterbot.corpus.english.greetings")
-chatbot.train("chatterbot.corpus.english.conversations")
+chatbot.train("chatterbot.corpus.english")
 query_answer = input(
     "Please specify whether you want to type(1) or talk(2) or experimental(3): ")
-if (query_answer == "2"):
+if (query_answer == 2):
     r = sr.Recognizer()
     m = sr.Microphone()
     print("A moment of silence, please...")
@@ -54,7 +53,9 @@ if (query_answer == "2"):
                 engine.say(response)
                 engine.runAndWait()
                 # time.sleep(2)
-elif(query_answer == "3"):
+elif(query_answer == 3):
+    print("Starting in 3 seconds")
+    time.sleep(3)
     r = sr.Recognizer()
     m = sr.Microphone()
     print("A moment of silence, please...")
@@ -68,7 +69,7 @@ elif(query_answer == "3"):
         def speak(a):
             engine.say(a)
             engine.runAndWait()
-            time.sleep(.5)
+            time.sleep(.2)
 
         def hear():
             audio = r.listen(source)
@@ -86,44 +87,55 @@ elif(query_answer == "3"):
         # engine.say('Hello there.')
         # engine.runAndWait()
         time.sleep(.5)
-        speak("My name is corinth!")
+        speak("Hello, My name is Corinth.")
         nameTruth = False
         name = "you"
         while not nameTruth:
-            speak("What is your name?")
+            speak("What, is your name?")
             item = hear()
-            prep = str("Your name is " + item + "? Is that correct?")
+            temp_name = item
+            prep = str("Your name is, " + item + "? Is that correct?")
             speak(prep)
+            loop0 = False
+            while not loop0:
+                item = hear()
+                # print("You: " + item)
+                if item == "yes":
+                    loop0 = True
+                    nameTruth = True
+                    name = temp_name
+                elif item == "no":
+                    speak("Sorry about that, let's try that again.")
+                    loop0 = True
+                else:
+                    speak("Sorry. I'm afraid I did not hear you properly. Is your name " +
+                          temp_name + "? Yes or No.")
+        prep = str("It is a pleasure to meet you, " + name)
+        speak(prep)
+        prep = str("Now, " + name + ". Please do not judge me too harshly. I am capable of many things, given my training dataset is sufficient. I am currently relying on a suboptimal dataset of movie quotes circa 2003. Given a larger dataset I am certain I could master the English language, but for now, I am what I am.")
+        speak(prep)
+        speak("Let's begin! You may speak when ready.")
+        iterations = 0
+        while iterations < 8:
             item = hear()
-            if item == "yes":
-                nameTruth = True
-                name = item
-            else:
-                speak("Sorry about that, let's try again.")
-        prep = str("It is a pleasure to meet you" + name)
-        speak(prep)
-        prep = str("Now " + name + ". Please do not judge me too harshly. I am capable of many things given my training dataset is sufficent. I am currently relying on a suboptimal dataset of movie quotes circa 2003. Given a larger dataset I am certain I could master the english language, but for now I am what I am.")
-        speak(prep)
-        while True:
-            audio = r.listen(source)
-            # try:
-            #     item = r.recognize_google(audio)
-            #     print("You: " + item)
-            # except sr.UnknownValueError:
-            #     print("Could not understand audio")
-            # except sr.RequestError as e:
-            #     print("Can't talk with servers right now; {0}".format(e))
             if "end connection" in str(item):
                 print("Very well, ending connection to Construct")
                 time.sleep(2)
                 exit()
+            elif item == "Error":
+                print("The system is having trouble hearing you.")
+            elif item == "Error b":
+                print(
+                    "The system cannot contact the servers right now, please address your networking issues first.")
             else:
                 response = chatbot.get_response("Chatbot: " + item)
                 print(response)
                 engine.say(response)
                 engine.runAndWait()
+                iterations = iterations + 1
                 # time.sleep(2)
-else:
+
+elif(query_answer == 1):
     # Get a response to an input statement
     while True:
         item = input("Type Message: ")
@@ -132,5 +144,7 @@ else:
             time.sleep(2)
             exit()
         else:
-            print(chatbot.get_response("" + item))
+            print(chatbot.get_response("Chatbot: " + item))
             time.sleep(2)
+else:
+    print("response not recognized, ending")
